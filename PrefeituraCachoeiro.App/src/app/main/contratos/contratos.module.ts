d71693c61d96f4744db5a819bb20e3b8d04640ee
IgorSadLino23 <igorsadlino@gmail.com>
2024-10-28 22:02:33 -0300

    Desenvolvimento tela contratos

--- PrefeituraCachoeiro.App/src/app/main/contratos/contratos.module.ts
+++ PrefeituraCachoeiro.App/src/app/main/contratos/contratos.module.ts
@@ -1,13 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContratosComponent } from './contratos.component';
-
-
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule} from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule} from '@angular/material/paginator';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { Editar_criar_contratosComponent } from './editar_criar_contratos/editar_criar_contratos.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [ ContratosComponent ],
+  declarations: [ ContratosComponent, Editar_criar_contratosComponent ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatFormFieldModule, 
+    MatInputModule,
+    MatTableModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatButtonModule,
+    MatDialogModule,
+    FormsModule 
   ]
 })
 export class ContratosModule { }
