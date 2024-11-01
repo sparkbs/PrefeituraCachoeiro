659ffad229c580b1acbd76bb7ce5462770af268f
IgorSadLino23 <igorsadlino@gmail.com>
2024-10-31 23:12:07 -0300

    ajuste codigo quebrado

--- PrefeituraCachoeiro.App/src/app/app.module.ts
+++ PrefeituraCachoeiro.App/src/app/app.module.ts
@@ -17,8 +17,8 @@ import { NavbarComponent } from './layout/components/navbar/navbar.component';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import { ConfiguracoesModule } from './main/configuracoes/configuracoes.module';
 import {MatCardModule} from '@angular/material/card';
-import { PerfilComponent } from './main/perfil/perfil.component';
 import { PerfilModule } from './main/perfil/perfil.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [ AppComponent, MainComponent, SidebarComponent, NavbarComponent ],
@@ -26,7 +26,8 @@ import { PerfilModule } from './main/perfil/perfil.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-
+    HttpClientModule,
+    
     // Autenticação
     LoginModule,
     RegistrarModule,
