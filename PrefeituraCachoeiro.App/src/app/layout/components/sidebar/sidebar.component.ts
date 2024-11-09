e89ddefad2f78287219b13e85f05e8e5d917c2f5
Roberto Oliveira <sparks01@wmi.solutions>
2024-11-09 12:56:34 -0300

    feat(app): Add aba relatorio

--- PrefeituraCachoeiro.App/src/app/layout/components/sidebar/sidebar.component.ts
+++ PrefeituraCachoeiro.App/src/app/layout/components/sidebar/sidebar.component.ts
@@ -11,6 +11,7 @@ import { AuthService } from 'src/app/services/auth.service';
 export class SidebarComponent {
   sidenavOpened: boolean = false;
   currentRoute: string = '';
+  relatoriosExpanded = false;
 
   constructor(
     private authService: AuthService,
@@ -36,6 +37,13 @@ export class SidebarComponent {
     const sidenavElement = this.el.nativeElement.querySelector('.sidebar');
     const sidenavButtonContentElement = this.el.nativeElement.querySelectorAll('.sidebar button > span');
     const sidenavButtonDescElement = this.el.nativeElement.querySelectorAll('.sidebar button > span > span');
+    const sidenavTextMenu = this.el.nativeElement.querySelectorAll('.menu div > span');
+    let sidenavIconExpandMenu = this.el.nativeElement.querySelector('.collapsed-menu');
+    const sidenavSubMenu = this.el.nativeElement.querySelectorAll('.submenu > button');
+
+    if (!sidenavIconExpandMenu) {
+      sidenavIconExpandMenu = this.el.nativeElement.querySelector('.expanded-menu');
+    }
 
     if (opened) {
       this.renderer.addClass(sidenavElement, 'sidebar-expanded');
@@ -49,9 +57,34 @@ export class SidebarComponent {
         sidenavButtonContentElement.forEach((span: HTMLSpanElement) => {
           span.style.width = '242px';
         });
+
+        sidenavTextMenu.forEach((span: HTMLSpanElement) => {
+          span.style.opacity = '1';
+          span.style.visibility = 'visible';
+          span.style.transition = 'all 0.35s';
+        });
+
+        this.renderer.removeClass(sidenavIconExpandMenu, 'collapsed-menu');
+        this.renderer.addClass(sidenavIconExpandMenu, 'expanded-menu');
+        if (sidenavSubMenu) {
+          sidenavSubMenu.forEach((button: HTMLElement) => {
+            button.style.opacity = '1';
+            button.style.visibility = 'visible';
+            button.style.transition = 'all 0.35s';
+          });
+        }
+
     }
     else {
       this.renderer.removeClass(sidenavElement, 'sidebar-expanded');
+      this.renderer.removeClass(sidenavIconExpandMenu, 'expanded-menu');
+      this.renderer.addClass(sidenavIconExpandMenu, 'collapsed-menu');
+      if (sidenavSubMenu) {
+        sidenavSubMenu.forEach((button: HTMLElement) => {
+          button.style.opacity = '0';
+          button.style.visibility = 'hidden';
+        });
+      }
 
       sidenavButtonDescElement.forEach((span: HTMLSpanElement) => {
         span.style.opacity = '0';
@@ -61,6 +94,15 @@ export class SidebarComponent {
       sidenavButtonContentElement.forEach((span: HTMLSpanElement) => {
         span.style.width = 'none';
       });
+
+      sidenavTextMenu.forEach((span: HTMLSpanElement) => {
+        span.style.opacity = '0';
+        span.style.visibility = 'hidden';
+      });
     }
+  }
+
+  toggleRelatorios() {
+    this.relatoriosExpanded = !this.relatoriosExpanded;
   }
 }
