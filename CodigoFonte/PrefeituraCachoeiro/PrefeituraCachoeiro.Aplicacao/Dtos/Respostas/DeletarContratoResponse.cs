1212e6553534544a5e89f02f5d772a708e2f03c1
sparkbs <aws@sparkbs.com.br>
2024-11-26 21:35:51 -0300

    Criação de rota excluir para a api de contrato

---
+++ CodigoFonte/PrefeituraCachoeiro/PrefeituraCachoeiro.Aplicacao/Dtos/Respostas/DeletarContratoResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace PrefeituraCachoeiro.Aplicacao.Dtos.Respostas
+{
+    [ExcludeFromCodeCoverage]
+    public class DeletarContratoResponse
+    {
+        public string Mensagem { get; set; } = null!;
+    }
+}
