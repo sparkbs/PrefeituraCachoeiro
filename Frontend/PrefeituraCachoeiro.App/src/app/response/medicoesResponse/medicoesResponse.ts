b28557616fc8da60d7bfbd61d75ada8568936f1e
Roberto Oliveira <robertooliveira@wmi.solutions>
2025-06-02 20:27:24 -0300

    ajuste tela modal soma total dos totais e filtro medicoes

--- Frontend/PrefeituraCachoeiro.App/src/app/response/medicoesResponse/medicoesResponse.ts
+++ Frontend/PrefeituraCachoeiro.App/src/app/response/medicoesResponse/medicoesResponse.ts
@@ -159,6 +159,7 @@ export class MedicoesResponse {
   }
 
   export class MedicaoLevantamento {
+    numeroMedicao: number;
     qtdItem: number;
     idItemContrato: number;
     valorComBdi: number;
