89027ac056f60dc3eb7fadab0abd3bb6426a754d
IgorSadLino23 <igorsadlino@gmail.com>
2025-07-08 21:07:13 -0300

    Ajuste api url

--- Frontend/PrefeituraCachoeiro.App/src/app/environments/Environments.ts
+++ Frontend/PrefeituraCachoeiro.App/src/app/environments/Environments.ts
@@ -1,7 +1,7 @@
 import * as CryptoJS from 'crypto-js';
 
 export const Environments = {
-    APIUrl: "https://prefeituraapi-enfyfagncef9djfc.brazilsouth-01.azurewebsites.net/v1/",
+    APIUrl: "https://minha-api-producao-dtfha5efafezfhe6.brazilsouth-01.azurewebsites.net/v1/",
     key: CryptoJS.enc.Utf8.parse('1234567890123456'), // 16 bytes para AES-128
     iv: CryptoJS.enc.Utf8.parse('6543210987654321')
 }
